6c442611f0a206ce929256592ce69faae7172c8e
Jason Ross <jaross@redhat.com>
2023-10-19 13:06:50 -0500

    fix(debug): add fs walk debug log

--- pkg/lib/file/file.go
+++ pkg/lib/file/file.go
@@ -2,13 +2,34 @@ package file
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
+
+	"github.com/machine-node-linker/manage-package/pkg/log"
 )
 
 func CheckFileVar(filename *string) error {
 	if _, err := os.Stat(*filename); *filename != "" && err != nil {
+		log.Debug.Printf("unable to read %s", *filename)
+		debugLogPaths()
+
 		return fmt.Errorf("unable to read file: %w", err)
 	}
 
 	return nil
+}
+
+func debugLogPaths() {
+	root := "/"
+	fileSystem := os.DirFS(root)
+
+	if err := fs.WalkDir(fileSystem, ".", func(path string, _ fs.DirEntry, err error) error {
+		if err != nil {
+			return nil
+		}
+		log.Debug.Println(path)
+		return nil
+	}); err != nil {
+		log.Debug.Printf("Unable to print fs: %v", err)
+	}
 }
